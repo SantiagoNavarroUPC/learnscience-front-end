6c10671f9afd933dab7263ab7e664200a62ecc2c
SantiagoNavarroUPC <snavarroc@unicesar.edu.co>
2024-10-20 11:42:28 -0500

    Recomendaciones QA

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -13,14 +13,9 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool _obscureText = true;
+  final _formKey = GlobalKey<FormState>();
 
-  @override
-  void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
-  @override
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -59,170 +54,180 @@ class _LoginScreenState extends State<LoginScreen> {
           Positioned.fill(
             child: SingleChildScrollView(
               padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 120),
-                  Container(
-                    width: 200, // Ajusta este valor para cambiar el tamaño
-                    height: 150, // Ajusta este valor para cambiar el tamaño
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/login.png'),
-                        fit: BoxFit.cover,
+              child: Form(
+                key: _formKey, // Asignamos la clave del formulario
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 120),
+                    // Imagen o logo
+                    Container(
+                      width: 200,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/login.png'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  ConstrainedBox(
-                    constraints: BoxConstraints(maxWidth: 400), // Limita el ancho máximo
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        // Título "Iniciar Sesión"
-                        const Text(
-                          'Iniciar Sesión',
-                          style: TextStyle(
-                            fontSize: 20, 
-                            fontFamily: 'Inter',
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        const Text(
-                          'Ingrese su dirección de correo electrónico y contraseña para acceder a la cuenta.',
-                          style: TextStyle(
-                            fontSize: 16, 
-                            fontFamily: 'Inter',
-                            color: Colors.black54,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 20),
-                        TextFormField(
-                          controller: emailController,
-                          decoration: const InputDecoration(
-                            labelText: 'Correo Electrónico',
-                            labelStyle: TextStyle(color: Colors.black),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: gColorTheme1_900),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
+                    ConstrainedBox(
+                      constraints: BoxConstraints(maxWidth: 400), // Limita el ancho máximo
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // Título "Iniciar Sesión"
+                          const Text(
+                            'Iniciar Sesión',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(color: Colors.black),
-                          cursorColor: Colors.teal,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Por favor ingrese el correo electrónico';
-                            }
-                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                              return 'Por favor ingrese un correo electrónico válido';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(height: 16),
-                        TextFormField(
-                          controller: passwordController,
-                          obscureText: _obscureText,
-                          decoration: InputDecoration(
-                            labelStyle: TextStyle(color: Colors.black),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: gColorTheme1_900),
+                          const SizedBox(height: 16),
+                          const Text(
+                            'Ingrese su dirección de correo electrónico y contraseña para acceder a la cuenta.',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'Inter',
+                              color: Colors.black54,
                             ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
-                            ),
-                            labelText: 'Contraseña',
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                _obscureText ? Icons.visibility : Icons.visibility_off,
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 20),
+                          // Campo de correo electrónico
+                          TextFormField(
+                            controller: emailController,
+                            decoration: const InputDecoration(
+                              labelText: 'Correo Electrónico',
+                              labelStyle: TextStyle(color: Colors.black),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: gColorTheme1_900),
                               ),
-                              onPressed: () {
-                                setState(() {
-                                  _obscureText = !_obscureText;
-                                });
-                              },
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                              ),
                             ),
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(color: Colors.black),
+                            cursorColor: Colors.teal,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor ingrese el correo electrónico';
+                              }
+                              if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                                return 'Por favor ingrese un correo electrónico válido';
+                              }
+                              return null;
+                            },
                           ),
-                          style: TextStyle(color: Colors.black),
-                          cursorColor: Colors.teal,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Por favor ingrese la contraseña';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        Obx(() {
-                          if (usuarioController.isLoading.value) {
-                            return const Center(child: CircularProgressIndicator());
-                          } else {
-                            return Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Expanded(
-                                      child: ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                          foregroundColor: gBackgroundColorSecundary,
-                                          backgroundColor: gColorTheme1_900,
-                                          padding: const EdgeInsets.symmetric(vertical: 24.0),
-                                          textStyle: const TextStyle(fontSize: 18, fontFamily:'Inter'),
-                                        ),
-                                        onPressed: () async {
-                                          final email = emailController.text;
-                                          final password = passwordController.text;
-                                          await usuarioController.loginUsuario(email, password);
-                                          if (usuarioController.usuario.value != null) {
-                                            Navigator.pushReplacementNamed(context, "/home");
-                                          } else {
-                                            Get.snackbar(
-                                              'Error',
-                                              usuarioController.errorMessage.value,
-                                              snackPosition: SnackPosition.BOTTOM,
-                                              backgroundColor: gColorTheme_Error,
-                                              colorText: Colors.white,
-                                            );
-                                          }
-                                          emailController.clear(); // Limpiar campos al ir a la lista
-                                          passwordController.clear();
-                                        },
-                                        child: const Text('Iniciar Sesión'),
-                                      ),
-                                    ),
-                                    const SizedBox(width: 16),
-                                    Expanded(
-                                      child: ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                          foregroundColor: gColorTheme1_900,
-                                          backgroundColor: gBackgroundColorSecundary,
-                                          padding: const EdgeInsets.symmetric(vertical: 24.0),
-                                          textStyle: const TextStyle(fontSize: 18),
-                                          side: const BorderSide(color: gColorTheme1_900, width: 2),
-                                        ),
-                                        onPressed: () {
-                                          emailController.clear();
-                                          passwordController.clear();
-                                          Get.toNamed('/registrarse');
-                                        },
-                                        child: const Text('Registrarse'),
-                                      ),
-                                    ),
-                                  ],
+                          const SizedBox(height: 16),
+                          // Campo de contraseña
+                          TextFormField(
+                            controller: passwordController,
+                            obscureText: _obscureText,
+                            decoration: InputDecoration(
+                              labelStyle: TextStyle(color: Colors.black),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: gColorTheme1_900),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                              ),
+                              labelText: 'Contraseña',
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _obscureText ? Icons.visibility : Icons.visibility_off,
                                 ),
-                              ],
-                            );
-                          }
-                        }),
-                      ],
+                                onPressed: () {
+                                  setState(() {
+                                    _obscureText = !_obscureText;
+                                  });
+                                },
+                              ),
+                            ),
+                            style: TextStyle(color: Colors.black),
+                            cursorColor: Colors.teal,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor ingrese la contraseña';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          // Botones de inicio de sesión y registro
+                          Obx(() {
+                            if (usuarioController.isLoading.value) {
+                              return const Center(child: CircularProgressIndicator());
+                            } else {
+                              return Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            foregroundColor: gBackgroundColorSecundary,
+                                            backgroundColor: gColorTheme1_900,
+                                            padding: const EdgeInsets.symmetric(vertical: 24.0),
+                                            textStyle: const TextStyle(fontSize: 18, fontFamily: 'Inter'),
+                                          ),
+                                          onPressed: () async {
+                                            if (_formKey.currentState!.validate()) {
+                                              // Si la validación es correcta
+                                              final email = emailController.text;
+                                              final password = passwordController.text;
+                                              await usuarioController.loginUsuario(email, password);
+                                              if (usuarioController.usuario.value != null) {
+                                                Navigator.pushReplacementNamed(context, "/home");
+                                              } else {
+                                                Get.snackbar(
+                                                  'Error',
+                                                  usuarioController.errorMessage.value,
+                                                  snackPosition: SnackPosition.BOTTOM,
+                                                  backgroundColor: gColorTheme_Error,
+                                                  colorText: Colors.white,
+                                                );
+                                              }
+                                              emailController.clear();
+                                              passwordController.clear();
+                                            }
+                                          },
+                                          child: const Text('Iniciar Sesión'),
+                                        ),
+                                      ),
+                                      const SizedBox(width: 16),
+                                      Expanded(
+                                        child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            foregroundColor: gColorTheme1_900,
+                                            backgroundColor: gBackgroundColorSecundary,
+                                            padding: const EdgeInsets.symmetric(vertical: 24.0),
+                                            textStyle: const TextStyle(fontSize: 18),
+                                            side: const BorderSide(color: gColorTheme1_900, width: 2),
+                                          ),
+                                          onPressed: () {
+                                            emailController.clear();
+                                            passwordController.clear();
+                                            Get.toNamed('/registrarse');
+                                          },
+                                          child: const Text('Registrarse'),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              );
+                            }
+                          }),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
@@ -230,5 +235,11 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 }
-  
